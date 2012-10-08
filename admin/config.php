732f5bf5eebe4a1071f673ae24a2005a4ec6a2a1
Petar Borovcanin <petar@ballab.com>
2012-10-08 14:23:06 +0200

    start

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/OPENCART_N/admin/');
+define('HTTP_CATALOG', 'http://localhost/OPENCART_N/');
+define('HTTP_IMAGE', 'http://localhost/OPENCART_N/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/OPENCART_N/admin/');
+define('HTTPS_CATALOG', 'http://localhost/OPENCART_N/');
+define('HTTPS_IMAGE', 'http://localhost/OPENCART_N/image/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\OPENCART_N/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\OPENCART_N/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\OPENCART_N/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\OPENCART_N/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\OPENCART_N/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\OPENCART_N/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\OPENCART_N/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\OPENCART_N/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\OPENCART_N/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\OPENCART_N/system/logs/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\OPENCART_N/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart_n');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/OPENCART_N/');
+define('HTTP_IMAGE', 'http://localhost/OPENCART_N/image/');
+define('HTTP_ADMIN', 'http://localhost/OPENCART_N/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/OPENCART_N/');
+define('HTTPS_IMAGE', 'http://localhost/OPENCART_N/image/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\OPENCART_N/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\OPENCART_N/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\OPENCART_N/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\OPENCART_N/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\OPENCART_N/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\OPENCART_N/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\OPENCART_N/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\OPENCART_N/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\OPENCART_N/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\OPENCART_N/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart_n');
+define('DB_PREFIX', '');
+?>
