40a09bb22b2d8196d91e574fbe73c396698538cb
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-08 16:42:38 +0200

    forgotten  file

--- catalog/language/serbian/account/forgotten.php
+++ catalog/language/serbian/account/forgotten.php
@@ -1,17 +1,17 @@
 <?php
 // Heading 
-$_['heading_title']   = 'Forgot Your Password?';
+$_['heading_title']   = 'Zaboravili ste lozinku?';
 
 // Text
-$_['text_account']    = 'Account';
-$_['text_forgotten']  = 'Forgotten Password';
-$_['text_your_email'] = 'Your E-Mail Address';
-$_['text_email']      = 'Enter the e-mail address associated with your account. Click submit to have your password e-mailed to you.';
-$_['text_success']    = 'Success: A new password has been sent to your e-mail address.';
+$_['text_account']    = 'Moj nalog';
+$_['text_forgotten']  = 'Zaboravljena lozinka';
+$_['text_your_email'] = 'Vaša email adresa';
+$_['text_email']      = 'Unesite email adresu povezanu sa vašim nalogom. Kliknite na dugme dalje za svoju lozinku koja vam je poslata emailom.';
+$_['text_success']    = 'Nova lozinka je poslata na vašu email adresu.';
 
 // Entry
-$_['entry_email']     = 'E-Mail Address:';
+$_['entry_email']     = 'E-Mail adresa:';
 
 // Error
-$_['error_email']     = 'Warning: The E-Mail Address was not found in our records, please try again!';
+$_['error_email']     = 'Upozorenje: Email adresa nije u našoj evidenciji, molimo Vas pokušajte ponovo!';
 ?>
