e18e34c92d45de2c90a2b261aba430d215e99ca4
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-08 15:39:45 +0200

    preveden asdress git ignore cache

--- catalog/language/serbian/account/address.php
+++ catalog/language/serbian/account/address.php
@@ -1,38 +1,38 @@
 <?php
 // Heading 
-$_['heading_title']     = 'Address Book';
+$_['heading_title']     = 'Adresar';
 
 // Text
-$_['text_account']      = 'Account';
-$_['text_address_book'] = 'Address Book Entries';
-$_['text_edit_address'] = 'Edit Address';
-$_['text_insert']       = 'Your address has been successfully inserted';
-$_['text_update']       = 'Your address has been successfully updated';
-$_['text_delete']       = 'Your address has been successfully deleted';
+$_['text_account']      = 'Nalog';
+$_['text_address_book'] = 'Unos u adresar';
+$_['text_edit_address'] = 'Izmeni adresu';
+$_['text_insert']       = 'Tvoja adresa je uspešno ubačena';
+$_['text_update']       = 'Tvoja adresa je uspešno izmenjena';
+$_['text_delete']       = 'Tvoja adresa je uspešno obrisana';
 
 // Entry
-$_['entry_firstname']   = 'First Name:';
-$_['entry_lastname']    = 'Last Name:';
-$_['entry_company']     = 'Company:';
+$_['entry_firstname']   = 'Ime:';
+$_['entry_lastname']    = 'Prezime:';
+$_['entry_company']     = 'Firma:';
 $_['entry_company_id']  = 'Company ID:';
 $_['entry_tax_id']      = 'Tax ID:';
-$_['entry_address_1']   = 'Address 1:';
-$_['entry_address_2']   = 'Address 2:';
-$_['entry_postcode']    = 'Post Code:';
-$_['entry_city']        = 'City:';
-$_['entry_country']     = 'Country:';
-$_['entry_zone']        = 'Region / State:';
-$_['entry_default']     = 'Default Address:';
+$_['entry_address_1']   = 'Adresa 1:';
+$_['entry_address_2']   = 'Adresa 2:';
+$_['entry_postcode']    = 'Poštanski broj:';
+$_['entry_city']        = 'Grad:';
+$_['entry_country']     = 'Zemlja:';
+$_['entry_zone']        = 'Region / Država:';
+$_['entry_default']     = 'Zvanična adresa:';
 
 // Error
-$_['error_delete']      = 'Warning: You must have at least one address!';
-$_['error_default']     = 'Warning: You can not delete your default address!';
-$_['error_firstname']   = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']    = 'Last Name must be between 1 and 32 characters!';
-$_['error_vat']         = 'VAT number is invalid!';
-$_['error_address_1']   = 'Address must be between 3 and 128 characters!';
-$_['error_postcode']    = 'Postcode must be between 2 and 10 characters!';
-$_['error_city']        = 'City must be between 2 and 128 characters!';
-$_['error_country']     = 'Please select a country!';
-$_['error_zone']        = 'Please select a region / state!';
+$_['error_delete']      = 'Upozorenje: Morate imati bar jednu adresu!';
+$_['error_default']     = 'Upozorenje: Ne možete obrisati svoju zvaničnu adresu!';
+$_['error_firstname']   = 'Ime mora imati izmedju 1 i 32 karaktera!';
+$_['error_lastname']    = 'Prezime mora imati izmedju 1 and 32 karaktera!';
+$_['error_vat']         = 'VAT broj je pogrešan!';
+$_['error_address_1']   = 'Adresa mora imati izmedju  3 i 128 karaktera!';
+$_['error_postcode']    = 'Poštanski kod mora imati izmedju  2 i 10 karaktera!';
+$_['error_city']        = 'Grad mora imati izmedju  2 i 128 karaktera!';
+$_['error_country']     = 'Molimo Vas označite zemlju!';
+$_['error_zone']        = 'Molimo Vas označite region / državu!';
 ?>
