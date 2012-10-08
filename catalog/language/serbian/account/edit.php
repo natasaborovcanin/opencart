34890109c646a8e2939c2e108da5b8e1551fd21b
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-08 16:32:14 +0200

    preveden edit file

--- catalog/language/serbian/account/edit.php
+++ catalog/language/serbian/account/edit.php
@@ -1,24 +1,24 @@
 <?php
 // Heading 
-$_['heading_title']     = 'My Account Information';
+$_['heading_title']     = 'Informacije o mom nalogu';
 
 // Text
-$_['text_account']      = 'Account';
-$_['text_edit']         = 'Edit Information';
-$_['text_your_details'] = 'Your Personal Details';
-$_['text_success']      = 'Success: Your account has been successfully updated.';
+$_['text_account']      = 'Moj nalog';
+$_['text_edit']         = 'Izmeni podatke';
+$_['text_your_details'] = 'Tvoji lični podaci';
+$_['text_success']      = 'Tvoj nalog je uspešno izmenjen.';
 
 // Entry
-$_['entry_firstname']  = 'First Name:';
-$_['entry_lastname']   = 'Last Name:';
+$_['entry_firstname']  = 'Ime:';
+$_['entry_lastname']   = 'Prezime:';
 $_['entry_email']      = 'E-Mail:';
-$_['entry_telephone']  = 'Telephone:';
+$_['entry_telephone']  = 'Telefon:';
 $_['entry_fax']        = 'Fax:';
 
 // Error
-$_['error_exists']     = 'Warning: E-Mail address is already registered!';
-$_['error_firstname']  = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']   = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']      = 'E-Mail Address does not appear to be valid!';
-$_['error_telephone']  = 'Telephone must be between 3 and 32 characters!';
+$_['error_exists']     = 'Upozorenje: E-Mail adresa je već registrovana!';
+$_['error_firstname']  = 'Ime mora biti izmedju 1 i 32 karaktera!';
+$_['error_lastname']   = 'prezime mora biti izmedju 1 i 32 karaktera!';
+$_['error_email']      = 'E-Mail adresa nije ispravna!';
+$_['error_telephone']  = 'Telefon mora biti izmedju 3 i 32 karaktera!';
 ?>
