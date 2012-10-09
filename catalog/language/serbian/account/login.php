9cd824d3e5b37c757437e503eccf8046eb5316fc
Natasa Borovcanin <natasaborovcanin@gmail.com>
2012-10-09 14:52:45 +0200

    login.php polovican prevod

--- catalog/language/serbian/account/login.php
+++ catalog/language/serbian/account/login.php
@@ -1,11 +1,11 @@
 <?php
 // Heading 
-$_['heading_title']                = 'Account Login';
+$_['heading_title']                = 'Prijava na moj nalog';
 
 // Text
-$_['text_account']                 = 'Account';
-$_['text_login']                   = 'Login';
-$_['text_new_customer']            = 'New Customer';
+$_['text_account']                 = 'Moj nalog';
+$_['text_login']                   = 'Prijava';
+$_['text_new_customer']            = 'Novi ';
 $_['text_register']                = 'Register Account';
 $_['text_register_account']        = 'By creating an account you will be able to shop faster, be up to date on an order\'s status, and keep track of the orders you have previously made.';
 $_['text_returning_customer']      = 'Returning Customer';
