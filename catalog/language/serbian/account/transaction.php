089714dfdb941fe795c3e0526a8f5eac6ce8ee5b
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-09 17:28:00 +0200

    transaction

--- catalog/language/serbian/account/transaction.php
+++ catalog/language/serbian/account/transaction.php
@@ -1,15 +1,15 @@
 <?php
 // Heading 
-$_['heading_title']      = 'Your Transactions';
+$_['heading_title']      = 'Vaše transakcije';
 
 // Column
-$_['column_date_added']  = 'Date Added';
-$_['column_description'] = 'Description';
-$_['column_amount']      = 'Amount (%s)';
+$_['column_date_added']  = 'Datum';
+$_['column_description'] = 'Opis';
+$_['column_amount']      = 'Iznos (%s)';
 
 // Text
 $_['text_account']       = 'Account';
-$_['text_transaction']   = 'Your Transactions';
-$_['text_total']         = 'Your current balance is:';
-$_['text_empty']         = 'You do not have any transactions!';
+$_['text_transaction']   = 'Vaše transakcije';
+$_['text_total']         = 'Vaš promet ukupno je:';
+$_['text_empty']         = 'Nemate transakcija!';
 ?>
