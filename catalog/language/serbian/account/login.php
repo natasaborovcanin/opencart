4c9b54183559e32b3bb62a9e63e11a6740d60aef
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-09 15:31:55 +0200

    LOGIN

--- catalog/language/serbian/account/login.php
+++ catalog/language/serbian/account/login.php
@@ -5,18 +5,18 @@ $_['heading_title']                = 'Prijava na moj nalog';
 // Text
 $_['text_account']                 = 'Moj nalog';
 $_['text_login']                   = 'Prijava';
-$_['text_new_customer']            = 'Novi ';
-$_['text_register']                = 'Register Account';
-$_['text_register_account']        = 'By creating an account you will be able to shop faster, be up to date on an order\'s status, and keep track of the orders you have previously made.';
-$_['text_returning_customer']      = 'Returning Customer';
-$_['text_i_am_returning_customer'] = 'I am a returning customer';
-$_['text_forgotten']               = 'Forgotten Password';
+$_['text_new_customer']            = 'Novi korisnik';
+$_['text_register']                = 'Kreiranje naloga';
+$_['text_register_account']        = 'Kreiranjem naloga bićete u mogućnosti da kupujete brže, da pratite status porudžbine, i imate uvid u prethodne porudžbine.';
+$_['text_returning_customer']      = 'Stari korisnik';
+$_['text_i_am_returning_customer'] = 'Ja sam stari korisnik';
+$_['text_forgotten']               = 'Zaboravljena lozinka';
 
 // Entry
-$_['entry_email']                  = 'E-Mail Address:';
-$_['entry_password']               = 'Password:';
+$_['entry_email']                  = 'E-Mail adresa:';
+$_['entry_password']               = 'Lozinka:';
 
 // Error
-$_['error_login']                  = 'Warning: No match for E-Mail Address and/or Password.';
-$_['error_approved']               = 'Warning: Your account requires approval before you can login.'; 
+$_['error_login']                  = 'Upozorenje: Neodgovarajuća E-Mail adresa i/ili lozinka.';
+$_['error_approved']               = 'Upozorenje: Vaš nalog zahteva odobrenje pre nego što se ulogujete.'; 
 ?>
