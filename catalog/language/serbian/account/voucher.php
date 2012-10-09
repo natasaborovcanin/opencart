4ce16e4f1f3cff3ce9e4dbd1f40285e3b594cbcf
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-09 17:43:23 +0200

    voucher

--- catalog/language/serbian/account/voucher.php
+++ catalog/language/serbian/account/voucher.php
@@ -1,29 +1,29 @@
 <?php
 // Heading 
-$_['heading_title']    = 'Purchase a Gift Certificate';
+$_['heading_title']    = 'Porudžbeni poklon certifikat';
 
 // Text
-$_['text_account']     = 'Account';
-$_['text_voucher']     = 'Gift Voucher';
-$_['text_description'] = 'This gift certificate will be emailed to the recipient after your order has been paid for.';
-$_['text_agree']       = 'I understand that gift certificates are non-refundable.';
-$_['text_message']     = '<p>Thank you for purchasing a gift certificate! Once you have completed your order your gift voucher recipient will be sent an email with details how to redeem their gift voucher.</p>';
-$_['text_for']         = '%s Gift Certificate for %s';
+$_['text_account']     = 'Moj nalog';
+$_['text_voucher']     = 'Poklon certifikat';
+$_['text_description'] = 'Ovaj poklon certifikat beće poslat emailom primaocu nakon što vaša porudžbina bude plaćena.';
+$_['text_agree']       = 'Razumem da poklon certifikat nemože da se refundira.';
+$_['text_message']     = '<p>Hvala vam što se poručili poklon certifikat! Kada kompletirate svoju porudžbinu your gift voucher reciprimalac će biti obavešten mailom o detaljima kako da otkupi svoj poklon vaučer.</p>';
+$_['text_for']         = '%s Poklon certifikat za %s';
 
 // Entry
-$_['entry_to_name']    = 'Recipient\'s Name:';
-$_['entry_to_email']   = 'Recipient\'s Email:';
-$_['entry_from_name']  = 'Your Name:';
-$_['entry_from_email'] = 'Your Email:';
-$_['entry_theme']      = 'Gift Certificate Theme:';
-$_['entry_message']    = 'Message:<br /><span class="help">(Optional)</span>';
-$_['entry_amount']     = 'Amount:<br /><span class="help">(Value must be between %s and %s)</span>';
+$_['entry_to_name']    = 'Primaočevo ime:';
+$_['entry_to_email']   = 'Primaočev Email:';
+$_['entry_from_name']  = 'Vaše ime:';
+$_['entry_from_email'] = 'Vaš Email:';
+$_['entry_theme']      = 'Tema poklon certifikata:';
+$_['entry_message']    = 'Poruka:<br /><span class="help">(Optional)</span>';
+$_['entry_amount']     = 'Iznos:<br /><span class="help">(Vrednost mora biti izmedju %s i %s)</span>';
 
 // Error
-$_['error_to_name']    = 'Recipient\'s Name must be between 1 and 64 characters!';
-$_['error_from_name']  = 'Your Name must be between 1 and 64 characters!';
-$_['error_email']      = 'E-Mail Address does not appear to be valid!';
-$_['error_theme']      = 'You must select a theme!';
-$_['error_amount']     = 'Amount must be between %s and %s!';
-$_['error_agree']      = 'Warning: You must agree that the gift certificates are non-refundable!';
+$_['error_to_name']    = 'Primaočevo ime mora biti izmedju 1 i 64 karaktera!';
+$_['error_from_name']  = 'vaše ime mora biti izmedju 1 i 64 karaktera!';
+$_['error_email']      = 'E-Mail adresa nije ispravna!';
+$_['error_theme']      = 'Morate izabrati temu!';
+$_['error_amount']     = 'Iznos mora biti izmedju %s i %s!';
+$_['error_agree']      = 'Upozorenje: Morate se složiti da poklon certifikat nemože biti refundiran!';
 ?>
