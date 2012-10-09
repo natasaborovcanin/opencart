cda8e8c0c20e56eef49d0620414089217d752e8b
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-09 15:43:47 +0200

    newsletter

--- catalog/language/serbian/account/newsletter.php
+++ catalog/language/serbian/account/newsletter.php
@@ -1,12 +1,12 @@
 <?php
 // Heading 
-$_['heading_title']    = 'Newsletter Subscription';
+$_['heading_title']    = 'Priva na obaveštenja o aktuelnostima';
 
 // Text
-$_['text_account']     = 'Account';
-$_['text_newsletter']  = 'Newsletter';
-$_['text_success']     = 'Success: Your newsletter subscription has been successfully updated!';
+$_['text_account']     = 'Moj nalog';
+$_['text_newsletter']  = 'Aktuelnosti';
+$_['text_success']     = 'Uspeh: Tvoja prijava na obaveštenja o aktuelnostima je uspešno izmenjena!';
 
 // Entry
-$_['entry_newsletter'] = 'Subscribe:';
+$_['entry_newsletter'] = 'Prijavi se:';
 ?>
