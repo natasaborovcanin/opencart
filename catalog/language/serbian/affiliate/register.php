9914bd653058fd0df55086c921621ff8b69ae74c
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-10 15:14:08 +0200

    affiliate /register

--- catalog/language/serbian/affiliate/register.php
+++ catalog/language/serbian/affiliate/register.php
@@ -1,59 +1,59 @@
 <?php
 // Heading 
-$_['heading_title']             = 'Affiliate Program';
+$_['heading_title']             = 'Partnerski program';
 
 // Text
-$_['text_account']              = 'Account';
-$_['text_register']             = 'Affiliate Register';
-$_['text_account_already']      = 'If you already have an account with us, please login at the <a href="%s">login page</a>.';
-$_['text_signup']               = 'To create an affiliate account, fill in the form below ensuring you complete all the required fields:';
-$_['text_your_details']         = 'Your Personal Details';
-$_['text_your_address']         = 'Your Address Details';
-$_['text_payment']              = 'Payment Information';
-$_['text_your_password']        = 'Your Password';
-$_['text_cheque']               = 'Cheque';
+$_['text_account']              = 'Moj nalog';
+$_['text_register']             = 'Registracija partnera';
+$_['text_account_already']      = 'Ako već imate nalog kod nas, molimo Vas prijavite se na <a href="%s">stranici za prijavu</a>.';
+$_['text_signup']               = 'Da bi kreirali partnerski nalog, popunite formu ispod, proverite da li ste popunili sva obavezna polja:';
+$_['text_your_details']         = 'Vaši lični podaci';
+$_['text_your_address']         = 'Podaci o vašoj adresi';
+$_['text_payment']              = 'Način plaćanja';
+$_['text_your_password']        = 'Vaša lozinka';
+$_['text_cheque']               = 'Ček';
 $_['text_paypal']               = 'PayPal';
-$_['text_bank']                 = 'Bank Transfer';
-$_['text_agree']                = 'I have read and agree to the <a class="colorbox" href="%s" alt="%s"><b>%s</b></a>';
+$_['text_bank']                 = 'Bankovni transfer';
+$_['text_agree']                = 'Pročitao sam i slažem se sa <a class="colorbox" href="%s" alt="%s"><b>%s</b></a>';
 
 // Entry
-$_['entry_firstname']           = 'First Name:';
-$_['entry_lastname']            = 'Last Name:';
+$_['entry_firstname']           = 'Ime:';
+$_['entry_lastname']            = 'Prezime:';
 $_['entry_email']               = 'E-Mail:';
-$_['entry_telephone']           = 'Telephone:';
+$_['entry_telephone']           = 'Telefon:';
 $_['entry_fax']                 = 'Fax:';
-$_['entry_company']             = 'Company:';
-$_['entry_website']             = 'Web Site:';
-$_['entry_address_1']           = 'Address 1:';
-$_['entry_address_2']           = 'Address 2:';
-$_['entry_postcode']            = 'Post Code:';
-$_['entry_city']                = 'City:';
-$_['entry_country']             = 'Country:';
-$_['entry_zone']                = 'Region / State:';
-$_['entry_tax']                 = 'Tax ID:';
-$_['entry_payment']             = 'Payment Method:';
-$_['entry_cheque']              = 'Cheque Payee Name:';
-$_['entry_paypal']              = 'PayPal Email Account:';
-$_['entry_bank_name']           = 'Bank Name:';
-$_['entry_bank_branch_number']  = 'ABA/BSB number (Branch Number):';
-$_['entry_bank_swift_code']     = 'SWIFT Code:';
-$_['entry_bank_account_name']   = 'Account Name:';
-$_['entry_bank_account_number'] = 'Account Number:';
-$_['entry_password']            = 'Password:';
-$_['entry_confirm']             = 'Password Confirm:';
+$_['entry_company']             = 'Firma:';
+$_['entry_website']             = 'Web sajt:';
+$_['entry_address_1']           = 'Adresa 1:';
+$_['entry_address_2']           = 'Adresa 2:';
+$_['entry_postcode']            = 'Poštanski broj:';
+$_['entry_city']                = 'Grad:';
+$_['entry_country']             = 'Zemlja:';
+$_['entry_zone']                = 'Region / Država:';
+$_['entry_tax']                 = 'Poreski broj:';
+$_['entry_payment']             = 'Način plaćanja:';
+$_['entry_cheque']              = 'Ime primaoca čeka:';
+$_['entry_paypal']              = 'PayPal Email nalog:';
+$_['entry_bank_name']           = 'Naziv banke:';
+$_['entry_bank_branch_number']  = 'ABA/BSB broj (Broj ogranka):';
+$_['entry_bank_swift_code']     = 'SWIFT kod:';
+$_['entry_bank_account_name']   = 'Naziv naloga:';
+$_['entry_bank_account_number'] = 'Broj naloga:';
+$_['entry_password']            = 'Lozinka:';
+$_['entry_confirm']             = 'Potvrda lozinke:';
 
 // Error
-$_['error_exists']              = 'Warning: E-Mail Address is already registered!';
-$_['error_firstname']           = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']            = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']               = 'E-Mail Address does not appear to be valid!';
-$_['error_telephone']           = 'Telephone must be between 3 and 32 characters!';
-$_['error_password']            = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']             = 'Password confirmation does not match password!';
-$_['error_address_1']           = 'Address 1 must be between 3 and 128 characters!';
-$_['error_city']                = 'City must be between 2 and 128 characters!';
-$_['error_country']             = 'Please select a country!';
-$_['error_zone']                = 'Please select a region / state!';
-$_['error_postcode']            = 'Postcode must be between 2 and 10 characters!';
-$_['error_agree']               = 'Warning: You must agree to the %s!';
+$_['error_exists']              = 'Upozorenje: E-Mail adresa je već registrovana!';
+$_['error_firstname']           = 'Ime mora biti izmedju 1 i 32 karaktera!';
+$_['error_lastname']            = 'Prezime mora biti izmedju 1 i 32 karaktera!';
+$_['error_email']               = 'E-Mail adresa nije ispravna!';
+$_['error_telephone']           = 'Telefon mora biti izmedju 3 i 32 karaktera!';
+$_['error_password']            = 'Lozinka mora biti izmedju 4 i 20 karaktera!';
+$_['error_confirm']             = 'Potvrda lozinke ne odgovara lozinci!';
+$_['error_address_1']           = 'Adresa 1 mora biti izmedju 3 i 128 karaktera!';
+$_['error_city']                = 'Grad mora biti izmedju 2 i 128 karaktera!';
+$_['error_country']             = 'Molimo Vas odaberite zemlju!';
+$_['error_zone']                = 'Molimo Vas označite a region / državu!';
+$_['error_postcode']            = 'Poštanski broj mora biti izmedju 2 i 10 karaktera!';
+$_['error_agree']               = 'Upozorenje: Morate se složiti sa %s!';
 ?>
