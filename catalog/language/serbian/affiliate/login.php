4db1790361896b088da7eb08f15f0e880f7e668c
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-10 14:26:06 +0200

    affiliate /login

--- catalog/language/serbian/affiliate/login.php
+++ catalog/language/serbian/affiliate/login.php
@@ -1,22 +1,22 @@
 <?php
 // Heading 
-$_['heading_title']                 = 'Affiliate Program';
+$_['heading_title']                 = 'Partnerski program';
 
 // Text
-$_['text_account']                  = 'Account';
-$_['text_login']                    = 'Login';
-$_['text_description']              = '<p>%s affiliate program is free and enables members to earn revenue by placing a link or links on their web site which advertises %s or specific products on it. Any sales made to customers who have clicked on those links will earn the affiliate commission. The standard commission rate is currently %s.</p><p>For more information, visit our FAQ page or see our Affiliate terms &amp; conditions.</p>';
-$_['text_new_affiliate']            = 'New Affiliate';
-$_['text_register_account']         = '<p>I am not currently an affiliate.</p><p>Click Continue below to create a new affiliate account. Please note that this is not connected in any way to your customer account.</p>';
-$_['text_returning_affiliate']      = 'Affiliate Login';
-$_['text_i_am_returning_affiliate'] = 'I am a returning affiliate.';
-$_['text_forgotten']                = 'Forgotten Password';
+$_['text_account']                  = 'Moj nalog';
+$_['text_login']                    = 'Prijava';
+$_['text_description']              = '<p>%s Partnerski program je besplatan i omogućava članovima da stekne prihod postavljanjem linka ili linkova na njihovim web sajtovima koji oglašavaju %s ili specifičnih produkata na njima. Svaka prodaja prema mušterijama koje su kliknule na te linkove zaradiće partnersku proviziju. Standardna provizija je trenutno %s.</p><p>Za više informacija, posetite našu staranicu za pitanja ili pogledajte naša partneska pravila   i uslove.</p>';
+$_['text_new_affiliate']            = 'Novi partner';
+$_['text_register_account']         = '<p>Trenutno nisam partner.</p><p>kliknite na dalje ispod da bi kreirali novi partnerski nalog. Molimo Vas imajte na umu da ovo ni na koji način nije povezano sa vašim nalogom kao mušterija.</p>';
+$_['text_returning_affiliate']      = 'Partnerska prijava';
+$_['text_i_am_returning_affiliate'] = 'Ja sam registrovani partner.';
+$_['text_forgotten']                = 'Zaboravljena lozinka';
 
 // Entry
-$_['entry_email']                   = 'Affiliate E-Mail:';
-$_['entry_password']                = 'Password:';
+$_['entry_email']                   = 'Partnerski email:';
+$_['entry_password']                = 'Lozinka:';
 
 // Error
-$_['error_login']                   = 'Warning: No match for E-Mail Address and/or Password.';
-$_['error_approved']                = 'Warning: Your account requires approval before you can login.';
+$_['error_login']                   = 'Upozorenje: Pogrešna je email adresa i/ili lozinka.';
+$_['error_approved']                = 'Upozorenje: Vašem nalogu je potrebno odobrenje pre nego što  možete da se ulogujete.';
 ?>
