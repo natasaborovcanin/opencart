0f80b30e2da909dbde83b1656338ff0ebc1a039b
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-12 17:18:00 +0200

    mail/voucher

--- catalog/language/serbian/mail/voucher.php
+++ catalog/language/serbian/mail/voucher.php
@@ -1,9 +1,9 @@
 <?php
 // Text
-$_['text_subject']  = 'You have been sent a gift voucher from %s';
-$_['text_greeting'] = 'Congratulations, You have received a Gift Certificate worth %s';
-$_['text_from']     = 'This Gift Certificate has been sent to you by %s';
-$_['text_message']  = 'With a message saying';
-$_['text_redeem']   = 'To redeem this Gift Certificate, write down the redemption code which is <b>%s</b> then click on the the link below and purchase the product you wish to use this gift voucher on. You can enter the gift voucher code on the shopping cart page before you click checkout.';
-$_['text_footer']   = 'Please reply to this email if you have any questions.';
+$_['text_subject']  = 'Dobili ste poklon vaučer od %s';
+$_['text_greeting'] = 'Čestitamo, primili ste poklon certifikat vredan  %s';
+$_['text_from']     = 'Ovaj poklon certifikat dobili ste od %s';
+$_['text_message']  = 'Sa sledećom porukom';
+$_['text_redeem']   = 'Da bi ste otkupili ovaj poklon certifikat, ispišite otkupni kod koji je <b>%s</b> zatim kliknite na link ispod i naručite proizvod na kom želite da iskoristite poklon vaučer. Možete uneti kod poklon vaučera na stranicu šoping kartica pre nego što se odjavite.';
+$_['text_footer']   = 'Molimo Vas odgovorite na ovaj mail ako imate bilo kakva pitanja.';
 ?>
