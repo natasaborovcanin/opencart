d0a8d0157942359ce17e92e53e5b4a3623d12174
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-12 15:42:15 +0200

    common/footer

--- catalog/language/serbian/common/footer.php
+++ catalog/language/serbian/common/footer.php
@@ -1,18 +1,18 @@
 <?php
 // Text
-$_['text_information']  = 'Information';
-$_['text_service']      = 'Customer Service';
-$_['text_extra']        = 'Extras';
-$_['text_contact']      = 'Contact Us';
-$_['text_return']       = 'Returns';
-$_['text_sitemap']      = 'Site Map';
-$_['text_manufacturer'] = 'Brands';
-$_['text_voucher']      = 'Gift Vouchers';
-$_['text_affiliate']    = 'Affiliates';
-$_['text_special']      = 'Specials';
-$_['text_account']      = 'My Account';
-$_['text_order']        = 'Order History';
-$_['text_wishlist']     = 'Wish List';
-$_['text_newsletter']   = 'Newsletter';
-$_['text_powered']      = 'Powered By <a href="http://www.opencart.com">OpenCart</a><br /> %s &copy; %s';
+$_['text_information']  = 'Informacije';
+$_['text_service']      = 'Korisnička podrška';
+$_['text_extra']        = 'Dodaci';
+$_['text_contact']      = 'Kontaktirajte nas';
+$_['text_return']       = 'Povraćaji';
+$_['text_sitemap']      = 'Mapa sajta';
+$_['text_manufacturer'] = 'Brendovi';
+$_['text_voucher']      = 'Poklon vaučeri';
+$_['text_affiliate']    = 'Partneri';
+$_['text_special']      = 'Specijalno';
+$_['text_account']      = 'Moj nalog';
+$_['text_order']        = 'Istorija porudžbina';
+$_['text_wishlist']     = 'Lista želja';
+$_['text_newsletter']   = 'Aktuelnosti';
+$_['text_powered']      = 'Bazirano na <a href="http://www.opencart.com">OpenCart</a><br /> %s &copy; %s';
 ?>
