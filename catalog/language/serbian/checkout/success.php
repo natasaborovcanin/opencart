2edeb3978643e01c265ba2581720cf902ce14d63
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-12 15:32:12 +0200

    checkout/success

--- catalog/language/serbian/checkout/success.php
+++ catalog/language/serbian/checkout/success.php
@@ -4,7 +4,7 @@ $_['heading_title'] = 'Vaša porudžbina je procesuirana!';
 $_['heading_title_customer'] = 'Vaša porudžbina #%s je obradjena!';
 
 // Text
-$_['text_customer'] = '<p>Vaša porudžbina je <a href="%s">#%s</a> je uspešno procesuirana!</p><p>Možete pogledati vašu istoriju narudžbina ako odete na <a href="%s">moj nalog</a> stranicu i klikom na <a href="%s">istorija</a>.</p><p>Ako vaša porudžbina ima povezani download, možete ići na nalog <a href="%s">downloads</a> stranicu da bi ste ih videli.</p><p>Ako imate bilo kakva pitanja uputite ih na <a href="%s">vlasnik šopa</a>.</p><p>Hvala Vam što ste kupovali sa nama online!</p>';
+$_['text_customer'] = '<p>Vaša porudžbina je <a href="%s">#%s</a> je uspešno procesuirana!</p><p>Možete pogledati vašu istoriju narudžbina ako odete na <a href="%s">moj nalog</a> stranicu i kliknete na <a href="%s">istorija</a>.</p><p>Ako vaša porudžbina ima povezani download, možete ići na nalog <a href="%s">downloads</a> stranicu da bi ste ih videli.</p><p>Ako imate bilo kakva pitanja uputite ih na <a href="%s">vlasnik šopa</a>.</p><p>Hvala Vam što ste kupovali sa nama online!</p>';
 $_['text_guest']    = '<p>Vaša porudžbina #%s je uspešno obradjena!</p><p>Ako imate bilo kakva pitanja molimo Vas uputite ih na <a href="%s">vlasnik šopa</a>.</p><p>Hvala Vam što kupujete sa nama online!</p>';
 $_['text_basket']   = 'Korpa';
 $_['text_checkout'] = 'Odjava';
