0476671935bf91e794079c6bbeaded7f3a4a302b
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-12 15:46:03 +0200

    common/header

--- catalog/language/serbian/common/header.php
+++ catalog/language/serbian/common/header.php
@@ -1,11 +1,11 @@
 <?php
 // Text
-$_['text_home']           = 'Home';
-$_['text_wishlist']       = 'Wish List (%s)';
-$_['text_shopping_cart']  = 'Shopping Cart';
-$_['text_search']         = 'Search';
-$_['text_welcome']        = 'Welcome visitor you can <a href="%s">login</a> or <a href="%s">create an account</a>.';
-$_['text_logged']         = 'You are logged in as <a href="%s">%s</a> <b>(</b> <a href="%s">Logout</a> <b>)</b>';
-$_['text_account']        = 'My Account';
-$_['text_checkout']       = 'Checkout';
+$_['text_home']           = 'Početna';
+$_['text_wishlist']       = 'Lista želja (%s)';
+$_['text_shopping_cart']  = 'Šoping kartica';
+$_['text_search']         = 'Traži';
+$_['text_welcome']        = 'Dobrodošao posetioče možeš se<a href="%s">prijaviti</a> ili <a href="%s">kreirati nalog</a>.';
+$_['text_logged']         = 'Prijavljeni ste kao <a href="%s">%s</a> <b>(</b> <a href="%s">odjava</a> <b>)</b>';
+$_['text_account']        = 'Moj nalog';
+$_['text_checkout']       = 'Odjava';
 ?>
