399e5df52e8a5468ebe306dc053ebdd75c861781
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-12 16:43:15 +0200

    mail/customer

--- catalog/language/serbian/mail/customer.php
+++ catalog/language/serbian/mail/customer.php
@@ -1,9 +1,9 @@
 <?php
 // Text
-$_['text_subject']  = '%s - Thank you for registering';
-$_['text_welcome']  = 'Welcome and thank you for registering at %s!';
-$_['text_login']    = 'Your account has now been created and you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_approval'] = 'Your account must be approved before you can login. Once approved you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_services'] = 'Upon logging in, you will be able to access other services including reviewing past orders, printing invoices and editing your account information.';
-$_['text_thanks']   = 'Thanks,';
+$_['text_subject']  = '%s - Hvala Vam što ste se registrovali';
+$_['text_welcome']  = 'Dobrodošli i hvala Vam za registraciju na %s!';
+$_['text_login']    = 'Vaš nalog je kreiran možete se prijaviti koristeći email adresu i lozinku posetom našem veb sajtu ili sledeći sledeći URL:';
+$_['text_approval'] = 'Vaš nalog mora biti odobren pre nego što budete u mogućnosti da se prijavite. Jednom kada vam nalog bude odobren možete se prijaviti koristeći svoju email adresu i lozinku posetom našem vebsajtu ili sledeći sledeći URL:';
+$_['text_services'] = 'Kad se prijavite, bićete u mogućnosti da pristupite drugim servisima uključujući pregled prošlih porudžbina, štampanje faktura i izmenu vaših detalja naloga.';
+$_['text_thanks']   = 'Hvala,';
 ?>
