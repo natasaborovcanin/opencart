80b780607213fc4c44c06d43a04f087ce89878c1
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-12 15:08:16 +0200

    checkout/manual

--- catalog/language/serbian/checkout/manual.php
+++ catalog/language/serbian/checkout/manual.php
@@ -1,29 +1,29 @@
 <?php
 // Text
-$_['text_for']          = '%s Gift Certificate for %s';
-$_['text_success']      = 'Order totals has been successfully re-calculated!';
+$_['text_for']          = '%s Poklon certifikat za %s';
+$_['text_success']      = 'Totali porudžbina su uspešno preračunati!';
 
 // Error
-$_['error_warning']     = 'Warning: Please check the form carefully for errors!';
-$_['error_permission']  = 'You do not have permission to access this page, please refer to your system administrator.';
-$_['error_customer']    = 'Warning: Can not find selected customer!';
-$_['error_email']       = 'E-Mail Address does not appear to be valid!';
-$_['error_postcode']    = 'Postcode must be between 2 and 10 characters!';
-$_['error_country']     = 'Please select a country!';
-$_['error_zone']        = 'Please select a region / state!';
-$_['error_stock']       = 'Products marked with *** are not available in the desired quantity or not in stock!';
-$_['error_minimum']     = 'Minimum order amount for %s is %s!';	
-$_['error_required']    = '%s required!';
-$_['error_shipping']    = 'Warning: Shipping method required!';
-$_['error_no_shipping'] = 'Warning: No Shipping options are available!';
-$_['error_payment']     = 'Warning: Payment method required!';
-$_['error_no_payment']  = 'Warning: No Payment options are available!';
-$_['error_coupon']      = 'Warning: Coupon is either invalid, expired or reached it\'s usage limit!';
-$_['error_voucher']     = 'Warning: Gift Voucher is either invalid or the balance has been used up!';
-$_['error_reward']      = 'Warning: Please enter the amount of reward points to use!';	
-$_['error_points']      = 'Warning: You don\'t have %s reward points!';
-$_['error_maximum']     = 'Warning: The maximum number of points that can be applied is %s!';
-$_['error_to_name']     = 'Recipient\'s Name must be between 1 and 64 characters!';
-$_['error_from_name']   = 'Your Name must be between 1 and 64 characters!';
-$_['error_amount']      = 'Amount must be between %s and %s!';
+$_['error_warning']     = 'Upozorenje: Molimo Vas da pažljivo proverite formu zbog grešaka!';
+$_['error_permission']  = 'Nemate dozvolu da pristupite ovoj stranici, molimo Vas obratite se vašem sistem administratoru.';
+$_['error_customer']    = 'Upozorenje: Nemožemo pronaći izabranu mušteriju!';
+$_['error_email']       = 'E-Mail Adresa nije ispravna!';
+$_['error_postcode']    = 'Poštanski broj mora biti izmedju 2 i 10 karaktera!';
+$_['error_country']     = 'Molimo Vas odaberite zemlju!';
+$_['error_zone']        = 'Molimo Vas odaberite region / državu!';
+$_['error_stock']       = 'Proizvodi označeni sa *** nisu dostupni u željenoj količini ili ih nema na zalihama!';
+$_['error_minimum']     = 'Minimalna količina za porudžbinu je %s is %s!';	
+$_['error_required']    = '%s potrebno uneti!';
+$_['error_shipping']    = 'Upozorenje: Potrebno je uneti metod transporta!';
+$_['error_no_shipping'] = 'Upozorenje: Načini transporta nisu dostupni!';
+$_['error_payment']     = 'Upozorenje: Potrebno je označiti metod plaćanja!';
+$_['error_no_payment']  = 'Upozorenje: Načini plaćanja nisu dostupni!';
+$_['error_coupon']      = 'Upozorenje: Kupon je ili neispravan, istekao ili dostigao svoj limit!';
+$_['error_voucher']     = 'Upozorenje: Poklon vaučer je ili neispravan ili iskorišten!';
+$_['error_reward']      = 'Upozorenje: Molimo Vas unesite količinu nagradnih poena koje želite da iskoristite!';	
+$_['error_points']      = 'Upozorenje: Nemate %s nagradnih poena!';
+$_['error_maximum']     = 'Upozorenje: Maksimalni broj poena koje možete iskoristiti je %s!';
+$_['error_to_name']     = 'Primaočevo ime mora sadržati izmedju 1 i 64 karaktera!';
+$_['error_from_name']   = 'Vaše ime mora sadržati izmedju 1 i 64 karaktera!';
+$_['error_amount']      = 'Iznos mora biti izmedju %s i %s!';
 ?>
