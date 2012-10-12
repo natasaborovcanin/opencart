ceb09f67c4cbb1fcdb7b61106623b2112dda0cef
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-12 16:18:02 +0200

    information/sitemap

--- catalog/language/serbian/information/sitemap.php
+++ catalog/language/serbian/information/sitemap.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']    = 'Site Map';
+$_['heading_title']    = 'Mapa sajta';
  
 // Text
-$_['text_special']     = 'Special Offers';
-$_['text_account']     = 'My Account';
-$_['text_edit']        = 'Account Information';
-$_['text_password']    = 'Password';
-$_['text_address']     = 'Address Book';
-$_['text_history']     = 'Order History';
+$_['text_special']     = 'Specijalne ponude';
+$_['text_account']     = 'Moj nalog';
+$_['text_edit']        = 'Informacije o nalogu';
+$_['text_password']    = 'Lozinka';
+$_['text_address']     = 'Adresar';
+$_['text_history']     = 'Istorija porudžbina';
 $_['text_download']    = 'Downloads';
-$_['text_cart']        = 'Shopping Cart';
-$_['text_checkout']    = 'Checkout';
-$_['text_search']      = 'Search';
-$_['text_information'] = 'Information';
-$_['text_contact']     = 'Contact Us';
+$_['text_cart']        = 'Šoping kartica';
+$_['text_checkout']    = 'Odjava';
+$_['text_search']      = 'Traži';
+$_['text_information'] = 'Informacije';
+$_['text_contact']     = 'Kontaktirajte nas';
 ?>
