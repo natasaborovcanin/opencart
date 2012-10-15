be5030310db597520e8d08e88f839fb915f05694
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-15 17:33:18 +0200

    module/special

--- catalog/language/serbian/module/special.php
+++ catalog/language/serbian/module/special.php
@@ -1,7 +1,7 @@
 <?php
 // Heading 
-$_['heading_title'] = 'Specials';
+$_['heading_title'] = 'Specijalno';
 
 // Text
-$_['text_reviews']  = 'Based on %s reviews.'; 
+$_['text_reviews']  = 'Bazirano na %s kritikama.'; 
 ?>
