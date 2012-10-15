fb4e95066a2ecb99c49617e29183caa5d3ae66a5
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-15 17:44:37 +0200

    payment/bank transfer

--- catalog/language/serbian/payment/bank_transfer.php
+++ catalog/language/serbian/payment/bank_transfer.php
@@ -1,7 +1,7 @@
 <?php
 // Text
-$_['text_title']       = 'Bank Transfer';
-$_['text_instruction'] = 'Bank Transfer Instructions';
-$_['text_description'] = 'Please transfer the total amount to the following bank account.';
-$_['text_payment']     = 'Your order will not ship until we receive payment.';
+$_['text_title']       = 'Bankovni transfer';
+$_['text_instruction'] = 'Instrukcije za bankovni transfer';
+$_['text_description'] = 'Molimo Vas prebacite ukupan iznos na sledeći bankovni račun.';
+$_['text_payment']     = 'Vaša porudžbina neće biti poslata  dok ne primimo uplatu.';
 ?>
