d2f99a063aa48d3db1e8595897c855032f651299
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-15 17:48:54 +0200

    payment/cheque

--- catalog/language/serbian/payment/cheque.php
+++ catalog/language/serbian/payment/cheque.php
@@ -1,8 +1,8 @@
 <?php
 // Text
-$_['text_title']       = 'Cheque / Money Order Instructions';
-$_['text_instruction'] = 'Cheque / Money Order';
-$_['text_payable']     = 'Make Payable To: ';
-$_['text_address']     = 'Send To: ';
-$_['text_payment']     = 'Your order will not ship until we receive payment.';
+$_['text_title']       = 'Ček / Novac instrukcije za porudžbinu';
+$_['text_instruction'] = 'Ček / Novac porudžbina';
+$_['text_payable']     = 'Naplativo za: ';
+$_['text_address']     = 'Pošalji: ';
+$_['text_payment']     = 'Vaša porudžbina neće biti isporučena dok ne primimo uplatu.';
 ?>
