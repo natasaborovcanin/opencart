6883fd795e984d1a693fae834bf03478737399b6
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-15 17:18:22 +0200

    module/affiliate

--- catalog/language/serbian/module/affiliate.php
+++ catalog/language/serbian/module/affiliate.php
@@ -1,16 +1,16 @@
 <?php
 // Heading 
-$_['heading_title']    = 'Affiliate';
+$_['heading_title']    = 'Partneri';
 
 // Text
-$_['text_register']    = 'Register';
-$_['text_login']       = 'Login';
-$_['text_logout']      = 'Logout';
-$_['text_forgotten']   = 'Forgotten Password';
-$_['text_account']     = 'My Account';
-$_['text_edit']        = 'Edit Account';
-$_['text_password']    = 'Password';
-$_['text_payment']     = 'Payment Options';
-$_['text_tracking']    = 'Affiliate Tracking';
-$_['text_transaction'] = 'Transactions';
+$_['text_register']    = 'Registracija';
+$_['text_login']       = 'Prijava';
+$_['text_logout']      = 'Odjava';
+$_['text_forgotten']   = 'Zaboravljena lozinka';
+$_['text_account']     = 'Moj nalog';
+$_['text_edit']        = 'Izmena naloga';
+$_['text_password']    = 'Lozinka';
+$_['text_payment']     = 'Opcije plaćanja';
+$_['text_tracking']    = 'Praćenje partnera';
+$_['text_transaction'] = 'Transakcije';
 ?>
