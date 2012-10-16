cbd05558303b4fd52d350ac358137a94b10aaf15
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-16 15:12:25 +0200

    payment/pp pro uk

--- catalog/language/serbian/payment/pp_pro_uk.php
+++ catalog/language/serbian/payment/pp_pro_uk.php
@@ -1,25 +1,25 @@
 <?php
 // Text
-$_['text_title']           = 'Credit or Debit Card (Processed securely by PayPal)';
-$_['text_credit_card']     = 'Credit Card Details';
-$_['text_start_date']      = '(if available)';
-$_['text_issue']           = '(for Maestro and Solo cards only)';
-$_['text_wait']            = 'Please wait!';
+$_['text_title']           = 'Kreditna ili debitna kartica (Procesuirano sigurno PayPal)';
+$_['text_credit_card']     = 'Podaci kreditne kartice';
+$_['text_start_date']      = '(ako je dostupno)';
+$_['text_issue']           = '(Samo za Maestro i Solo kartice)';
+$_['text_wait']            = 'Molimo Vas sačekajte!';
 
 // Entry
-$_['entry_cc_owner']       = 'Card Owner:';
-$_['entry_cc_type']        = 'Card Type:';
-$_['entry_cc_number']      = 'Card Number:';
-$_['entry_cc_start_date']  = 'Card Valid From Date:';
-$_['entry_cc_expire_date'] = 'Card Expiry Date:';
-$_['entry_cc_cvv2']        = 'Card Security Code (CVV2):';
-$_['entry_cc_issue']       = 'Card Issue Number:';
+$_['entry_cc_owner']       = 'Vlasnik kartice:';
+$_['entry_cc_type']        = 'Tip kartice:';
+$_['entry_cc_number']      = 'Broj kartice:';
+$_['entry_cc_start_date']  = 'Datum aktiviranja kartice:';
+$_['entry_cc_expire_date'] = 'Datum isteka kartice:';
+$_['entry_cc_cvv2']        = 'Sigurnosni kod kartice (CVV2):';
+$_['entry_cc_issue']       = 'Broj izdanja kartice:';
 
 // Error
-$_['error_required']       = 'Warning: All payment information fields are required.';
-$_['error_general']        = 'Warning: A general problem has occurred with the transaction. Please try again.';
-$_['error_config']         = 'Warning: Payment module configuration error. Please verify the login credentials.';
-$_['error_address']        = 'Warning: A match of the Payment Address City, State, and Postal Code failed. Please try again.';
-$_['error_declined']       = 'Warning: This transaction has been declined. Please try again.';
-$_['error_invalid']        = 'Warning: The provided credit card information is invalid. Please try again.';
+$_['error_required']       = 'Upozorenje: Sva polja sa informacijama o uplati moraju biti popunjena.';
+$_['error_general']        = 'Upozorenje: Nastao je problem prilikom transakcije. Molimo Vas pokušajte ponovo.';
+$_['error_config']         = 'Upozorenje: Greška konfiguracijskog modula plaćanja. Molimo Vas proverite akreditive prijave.';
+$_['error_address']        = 'Upozorenje: Podaci kod plaćanja za Adresu grad, državu, i poštanski broj nisu odgovarajući. Molimo Vas pokušajte ponovo.';
+$_['error_declined']       = 'Upozorenje: Ova transakcija je odbijena. Molimo Vas pokušajte ponovo.';
+$_['error_invalid']        = 'Upozorenje: Prosledjene informacije o kreditnoj kartici nisu ispravne. Molimo Vas pokušajte ponovo.';
 ?>
