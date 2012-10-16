b7ce877dc202b0c7c5c5f19cc151dcc18a55afeb
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-16 14:40:13 +0200

    payment/pp pro

--- catalog/language/serbian/payment/pp_pro.php
+++ catalog/language/serbian/payment/pp_pro.php
@@ -1,16 +1,16 @@
 <?php
 // Text
-$_['text_title']           = 'Credit or Debit Card (Processed securely by PayPal)';
-$_['text_credit_card']     = 'Credit Card Details';
-$_['text_start_date']      = '(if available)';
-$_['text_issue']           = '(for Maestro and Solo cards only)';
-$_['text_wait']            = 'Please wait!';
+$_['text_title']           = 'Kreditna ili debitna kartica (sigurno procesuirano PayPal)';
+$_['text_credit_card']     = 'Detalji kreditne kartice';
+$_['text_start_date']      = '(ako je dostupno)';
+$_['text_issue']           = '(Samo za Maestro i Solo kartice)';
+$_['text_wait']            = 'Molimo vas sačekajte!';
 
 // Entry
-$_['entry_cc_type']        = 'Card Type:';
-$_['entry_cc_number']      = 'Card Number:';
-$_['entry_cc_start_date']  = 'Card Valid From Date:';
-$_['entry_cc_expire_date'] = 'Card Expiry Date:';
-$_['entry_cc_cvv2']        = 'Card Security Code (CVV2):';
-$_['entry_cc_issue']       = 'Card Issue Number:';
+$_['entry_cc_type']        = 'Tip kartice:';
+$_['entry_cc_number']      = 'Broj kartice:';
+$_['entry_cc_start_date']  = 'Datum aktiviranja kartice:';
+$_['entry_cc_expire_date'] = 'Datum isteka kartice:';
+$_['entry_cc_cvv2']        = 'Sigurnosni kod kartice (CVV2):';
+$_['entry_cc_issue']       = 'Broj izdanja kartice:';
 ?>
