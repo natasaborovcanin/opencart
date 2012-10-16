83f78ccb098a5b0dbab474b113f3ca3e7e685e7c
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-16 13:39:55 +0200

    payment/klarna invoice

--- catalog/language/serbian/payment/klarna_invoice.php
+++ catalog/language/serbian/payment/klarna_invoice.php
@@ -1,23 +1,23 @@
 <?php
 // Text
-$_['text_title']       = 'Klarna Invoice';
-$_['text_information'] = 'Klarna Information';
-$_['text_additional']  = 'Klarna requires some additional information before they can proccess your order.';
-$_['text_wait']        = 'Please wait!';
-$_['text_male']        = 'Male';
-$_['text_female']      = 'Female';
+$_['text_title']       = 'Klarna faktura';
+$_['text_information'] = 'Klarna informacije';
+$_['text_additional']  = 'Klarna zahteva neke dodatne informacije pre nego što procesutra dalje vašu porudžbinu.';
+$_['text_wait']        = 'Molimo Vas sačekajte!';
+$_['text_male']        = 'Muško';
+$_['text_female']      = 'Žensko';
 
 // Entry
-$_['entry_gender']     = 'Gender:';
-$_['entry_pno']        = 'PNO / BIRTH DATA:<span class="help">(07071960)</span>';
-$_['entry_house_no']   = 'House No.:';
-$_['entry_house_ext']  = 'House Ext.:';
-$_['entry_cellno']     = 'Cell Phone:';
+$_['entry_gender']     = 'Pol:';
+$_['entry_pno']        = 'Datum rodjenja:<span class="help">(07071960)</span>';
+$_['entry_house_no']   = 'Kućni broj.:';
+$_['entry_house_ext']  = 'Kućna adresa:';
+$_['entry_cellno']     = 'Mobilni telefon:';
 
 // Error
-$_['error_gender']     = 'Gender Required!';
-$_['error_dob']        = 'Date of birth Required!';
-$_['error_house_no']   = 'House No. Required!';
-$_['error_house_ext']  = 'House Extension Required!';
-$_['error_dob']        = 'Date of birth Required!';
+$_['error_gender']     = 'Pol je obavezan!';
+$_['error_dob']        = 'Datum rodjenja je obavezan!';
+$_['error_house_no']   = 'Kućni broj je obavezan!';
+$_['error_house_ext']  = 'Kućna adresa je obavezna!';
+$_['error_dob']        = 'Datum rodjenja je obavezan!';
 ?>
