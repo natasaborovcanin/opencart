fac8e9ad11e6a430af42d2d0ccb19f65f6fe74ef
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-18 11:57:08 +0200

    shipping/royal mail

--- catalog/language/serbian/shipping/royal_mail.php
+++ catalog/language/serbian/shipping/royal_mail.php
@@ -1,18 +1,18 @@
 <?php
 // Text
 $_['text_title']                 = 'Royal Mail';
-$_['text_weight']                = 'Weight:';
-$_['text_insurance']             = 'Insured upto:';
-$_['text_1st_class_standard']    = 'First Class Standard Post';
-$_['text_1st_class_recorded']    = 'First Class Recorded Post';
-$_['text_2nd_class_standard']    = 'Second Class Standard Post';
-$_['text_2nd_class_recorded']    = 'Second Class Recorded Post';
-$_['text_special_delivery_500']  = 'Special Delivery Next Day (&pound;500)';
-$_['text_special_delivery_1000'] = 'Special Delivery Next Day (&pound;1000)';
-$_['text_special_delivery_2500'] = 'Special Delivery Next Day (&pound;2500)';
-$_['text_standard_parcels']      = 'Standard Parcels';
-$_['text_airmail']               = 'Airmail';
-$_['text_international_signed']  = 'International Signed';
-$_['text_airsure']               = 'Airsure';
-$_['text_surface']               = 'Surface';
+$_['text_weight']                = 'Težina:';
+$_['text_insurance']             = 'Osigurano do:';
+$_['text_1st_class_standard']    = 'Standardno slanje prvom klasom';
+$_['text_1st_class_recorded']    = 'Praćeno slanje prvom klasom';
+$_['text_2nd_class_standard']    = 'Standardno slanje drugom klasom';
+$_['text_2nd_class_recorded']    = 'Praćeno slanje drugom klasom';
+$_['text_special_delivery_500']  = 'Specijalna isporuka sledećeg dana (&pound;500)';
+$_['text_special_delivery_1000'] = 'Specijalna isporuka sledećeg dana (&pound;1000)';
+$_['text_special_delivery_2500'] = 'Specijalna isporuka sledećeg dana (&pound;2500)';
+$_['text_standard_parcels']      = 'Standardni paketi';
+$_['text_airmail']               = 'Vazdušna pošta';
+$_['text_international_signed']  = 'Medjunarodno potpisano';
+$_['text_airsure']               = 'Sigurno vazduhom';
+$_['text_surface']               = 'Površina';
 ?>
