e5d144c1a42349529895a7db6af427800d579e1a
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-24 17:15:47 +0200

    voucher

--- catalog/language/serbian/mail/voucher.php
+++ catalog/language/serbian/mail/voucher.php
@@ -4,6 +4,6 @@ $_['text_subject']  = 'Dobili ste poklon vaučer od %s';
 $_['text_greeting'] = 'Čestitamo, primili ste poklon certifikat vredan  %s';
 $_['text_from']     = 'Ovaj poklon certifikat dobili ste od %s';
 $_['text_message']  = 'Sa sledećom porukom';
-$_['text_redeem']   = 'Da bi ste otkupili ovaj poklon certifikat, ispišite otkupni kod koji je <b>%s</b> zatim kliknite na link ispod i naručite proizvod na kom želite da iskoristite poklon vaučer. Možete uneti kod poklon vaučera na stranicu šoping kartica pre nego što se odjavite.';
+$_['text_redeem']   = 'Da bi ste otkupili ovaj poklon certifikat, ispišite otkupni kod koji je <b>%s</b> zatim kliknite na link ispod i naručite proizvod na kom želite da iskoristite poklon vaučer. Možete uneti kod poklon vaučera na stranicu korpa pre nego što se odjavite.';
 $_['text_footer']   = 'Molimo Vas odgovorite na ovaj mail ako imate bilo kakva pitanja.';
 ?>
