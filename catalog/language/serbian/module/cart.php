bf607849e52d95bc60b3093521e5736bc08e36d9
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-24 17:17:07 +0200

    cart

--- catalog/language/serbian/module/cart.php
+++ catalog/language/serbian/module/cart.php
@@ -1,10 +1,10 @@
 <?php
 // Heading 
-$_['heading_title'] = 'Šoping kartica';
+$_['heading_title'] = 'Korpa';
 
 // Text
 $_['text_items']    = '%s proizvod(i) - %s';
-$_['text_empty']    = 'Vaša šoping kartica je prazna!';
+$_['text_empty']    = 'Vaša korpa je prazna!';
 $_['text_cart']     = 'Pogledajte karticu';
 $_['text_checkout'] = 'Odjava';
 ?>
