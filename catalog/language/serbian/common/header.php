2042c9a0acf2f905a6182dfbf1d5535d3c8c4294
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-24 17:14:23 +0200

    header

--- catalog/language/serbian/common/header.php
+++ catalog/language/serbian/common/header.php
@@ -2,7 +2,7 @@
 // Text
 $_['text_home']           = 'Početna';
 $_['text_wishlist']       = 'Lista želja (%s)';
-$_['text_shopping_cart']  = 'Šoping kartica';
+$_['text_shopping_cart']  = 'Korpa';
 $_['text_search']         = 'Traži';
 $_['text_welcome']        = 'Dobrodošao posetioče možeš se<a href="%s">prijaviti</a> ili <a href="%s">kreirati nalog</a>.';
 $_['text_logged']         = 'Prijavljeni ste kao <a href="%s">%s</a> <b>(</b> <a href="%s">odjava</a> <b>)</b>';
