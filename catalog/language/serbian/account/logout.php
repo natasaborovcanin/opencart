eec6b90a30c3c1c994e1f79310d78cf3cf834604
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-24 17:11:37 +0200

    logout

--- catalog/language/serbian/account/logout.php
+++ catalog/language/serbian/account/logout.php
@@ -3,7 +3,7 @@
 $_['heading_title'] = 'Odjava sa naloga';
 
 // Text
-$_['text_message']  = '<p>Odjavljeni ste sa svog naloga. Sad je sigurno da napustite računar.</p><p>Vaša šoping kartica je sačuvana, artikli unutar nje biće obnovljeni kad god se ulogujete u svoj nalog.</p>';
+$_['text_message']  = '<p>Odjavljeni ste sa svog naloga. Sad je sigurno da napustite računar.</p><p>Vaš sadr\aj korpe  je sačuvan, artikli unutar nje biće prikazani kad god se ulogujete u svoj nalog.</p>';
 $_['text_account']  = 'Moj nalog';
 $_['text_logout']   = 'Odjava';
 ?>
