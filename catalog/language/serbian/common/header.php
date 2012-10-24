d9f7fcfe71dfe674d5a310370b4c81b712acf9d0
Natasa Borovcanin <natasaborovcanin@yahoo.com>
2012-10-24 17:31:32 +0200

    prijaviti

--- catalog/language/serbian/common/header.php
+++ catalog/language/serbian/common/header.php
@@ -4,7 +4,7 @@ $_['text_home']           = 'Početna';
 $_['text_wishlist']       = 'Lista želja (%s)';
 $_['text_shopping_cart']  = 'Korpa';
 $_['text_search']         = 'Traži';
-$_['text_welcome']        = 'Dobrodošao posetioče možeš se<a href="%s">prijaviti</a> ili <a href="%s">kreirati nalog</a>.';
+$_['text_welcome']        = 'Dobrodošao posetioče možeš se<a href="%s"> prijaviti</a> ili <a href="%s">kreirati nalog</a>.';
 $_['text_logged']         = 'Prijavljeni ste kao <a href="%s">%s</a> <b>(</b> <a href="%s">odjava</a> <b>)</b>';
 $_['text_account']        = 'Moj nalog';
 $_['text_checkout']       = 'Odjava';
